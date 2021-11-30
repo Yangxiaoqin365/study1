6dbd569d4bc58ebd9609e660ec13750a07d32597
yangxiaoqin <17703705256@163.com>
2021-11-30 21:00:03 +0800

    super关键字

---
+++ studyWork/demo/src/main/java/com/it/yxq/string/StringDemo.java
@@ -0,0 +1,11 @@
+package com.it.yxq.string;
+
+/**
+ * @author yxq
+ * @data 2021/11/30 20:58
+ */
+public class StringDemo {
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
